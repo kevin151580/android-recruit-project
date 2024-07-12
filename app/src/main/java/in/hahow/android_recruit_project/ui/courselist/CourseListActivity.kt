2dfdd52047d26cd290ac7f602e53cd88827bce4d
Kevin <kevin151580@gmail.com>
2024-07-12 13:09:23 +0800

    feat: Add CourseListScreen

--- app/src/main/java/in/hahow/android_recruit_project/ui/courselist/CourseListActivity.kt
+++ app/src/main/java/in/hahow/android_recruit_project/ui/courselist/CourseListActivity.kt
@@ -5,7 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
 import dagger.hilt.android.AndroidEntryPoint
 import `in`.hahow.android_recruit_project.ui.theme.AndroidrecruitprojectTheme
 
@@ -19,6 +23,16 @@ class CourseListActivity : ComponentActivity() {
         setContent {
             val courseListUiState = courseListViewModel.courseListUiState.observeAsState()
             AndroidrecruitprojectTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    courseListUiState.value?.let {
+                        CourseListScreen(
+                            uiState = it,
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(innerPadding)
+                        )
+                    }
+                }
             }
         }
     }
